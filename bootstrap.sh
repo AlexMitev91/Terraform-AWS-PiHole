3aeb8714ac1381b88b576dd10583e401b6387658
Alexander <a.mitev91@gmail.com>
2023-06-12 01:01:27 +0300

    Updated provisioning script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,22 +3,24 @@
 #Update packages and hostname
 sudo apt update -y
 sudo apt upgrade -y
-sudo hostnamectl set-hostname pi-hole-serve
+sudo hostnamectl set-hostname pi-hole-server
 
 #Install common packages
-sudo apt install apt-transport-https ca-certificates curl software-properties-common git
+sudo apt install apt-transport-https ca-certificates curl software-properties-common git -y
 
 #Add Google nameservers and disable systemd-resolver
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+sudo echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 sudo systemctl stop systemd-resolved.service
 sudo systemctl disable systemd-resolved.service
 
 #Install docker 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update -y
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
+sudo systemctl start docker
+sudo systemctl enable docker
 
 # Download the compose file and start the container
-git clone https://github.com/AlexMitev91/Terraform-AWS-PiHole.git
-docker compose -f ~/Terraform-AWS-PiHole/docker-compose.yml up -d
+sudo git clone https://github.com/AlexMitev91/Terraform-AWS-PiHole.git /tmp/pi-hole
+sudo docker compose -f /tmp/pi-hole/docker-compose.yml up -d
