98e7b431065ae6bad6e5af569a6e07e71acbd3c2
Alexander <a.mitev91@gmail.com>
2023-06-29 00:22:08 +0300

    Cleaned up some commands

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -24,5 +24,4 @@ sudo systemctl enable docker
 # Download the compose file and start the container
 sudo mkdir -p mkdir /software/pi-hole
 sudo wget -O /software/pi-hole/docker-compose.yml  https://raw.github.com/AlexMitev91/Terraform-AWS-PiHole/main/docker-compose.yml
-#sudo git clone https://github.com/AlexMitev91/Terraform-AWS-PiHole.git /software/pi-hole
 sudo docker compose -f /software/pi-hole/docker-compose.yml up -d
